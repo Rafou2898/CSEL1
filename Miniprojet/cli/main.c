91c15ac299759b5398f05fd86ac9340c9c472f33
Rafou2898 <rafael.dousse@hes-so.ch>
2026-05-28 17:42:25 +0200

    fix: improved cli code

--- Miniprojet/cli/main.c
+++ Miniprojet/cli/main.c
@@ -7,28 +7,39 @@
 
 #define SOCKET_PATH "/tmp/fan.sock"
 
-int main(int argc, char* argv[]) {
+static void print_usage(const char* progname) {
+    fprintf(stderr, "Usage: %s <command> <value>\n", progname);
+    fprintf(stderr, "  mode <0|1>     -> 0=AUTO, 1=MANUAL\n");
+    fprintf(stderr, "  freq <0|1|2|3> -> 0=2Hz, 1=5Hz, 2=10Hz, 3=20Hz\n");
+}
+
+static int validate_args(int argc, char* argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s <command> <value>\n", argv[0]);
-        fprintf(stderr, "  mode <0|1>   -> 0=AUTO, 1=MANUAL\n");
-        fprintf(stderr, "  freq <0|1|2|3> -> 0=2Hz, 1=5Hz, 2=10Hz, 3=20Hz\n");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Error: missing arguments\n");
+        return -1;
     }
 
     if (strcmp(argv[1], "mode") != 0 && strcmp(argv[1], "freq") != 0) {
-        fprintf(stderr, "Invalid command: %s\n", argv[1]);
-
-        fprintf(stderr, "Usage: %s <command> <value>\n", argv[0]);
-        fprintf(stderr, "  mode <0|1>   -> 0=AUTO, 1=MANUAL\n");
-        fprintf(stderr, "  freq <0|1|2|3> -> 0=2Hz, 1=5Hz, 2=10Hz, 3=20Hz\n");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Error: unknown command '%s'\n", argv[1]);
+        return -1;
     }
 
-    if(strcmp(argv[2], "0") != 0 && strcmp(argv[2], "1") != 0 && strcmp(argv[2], "2") != 0 && strcmp(argv[2], "3") != 0) {
-        fprintf(stderr, "Invalid value: %s\n", argv[2]);
-        fprintf(stderr, "Usage: %s <command> <value>\n", argv[0]);
-        fprintf(stderr, "  mode <0|1>   -> 0=AUTO, 1=MANUAL\n");
-        fprintf(stderr, "  freq <0|1|2|3> -> 0=2Hz, 1=5Hz, 2=10Hz, 3=20Hz\n");
+    int val = atoi(argv[2]);
+    if (strcmp(argv[1], "mode") == 0 && (val < 0 || val > 1)) {
+        fprintf(stderr, "Error: mode value must be 0 or 1\n");
+        return -1;
+    }
+    if (strcmp(argv[1], "freq") == 0 && (val < 0 || val > 3)) {
+        fprintf(stderr, "Error: freq value must be 0, 1, 2 or 3\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (validate_args(argc, argv) != 0) {
+        print_usage(argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -51,13 +62,19 @@ int main(int argc, char* argv[]) {
 
     char msg[64];
     snprintf(msg, sizeof(msg), "%s %s", argv[1], argv[2]);
-    write(fd, msg, strlen(msg));
+    if (write(fd, msg, strlen(msg)) == -1) {
+        perror("write");
+        close(fd);
+        return EXIT_FAILURE;
+    }
 
     char response[256];
     ssize_t n = read(fd, response, sizeof(response) - 1);
     if (n > 0) {
         response[n] = '\0';
         printf("%s\n", response);
+    } else if (n == -1) {
+        perror("read");
     }
 
     close(fd);
